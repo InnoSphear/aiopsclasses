b195608046d67caaec77ff249f435d3089a108c4
InnoSphear <your-innosphear@gmail.com>
2026-07-13 21:51:34 +0530

    Fixed with the errors

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -1,4 +1,4 @@
-import Message from '../models/message.model.js';
+import Message from '../models/Message.model.js';
 import Batch from '../models/Batch.model.js';
 import { NotFoundError, ValidationError } from '../utils/apiError.js';
 
